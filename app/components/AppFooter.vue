7ccf738acee66a30c893c35c5f1185f4ff48bb3c
dos <dosdima89@gmail.com>
2026-02-17 14:31:23 +0300

    fix/home page

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,19 +1,48 @@
 <template>
   <footer class="footer">
     <div class="footer__inner">
-      <div class="footer__links">
-        <a class="footer__link" href="#">Политика конфиденциальности</a>
-        <a class="footer__link" href="#">Пользовательское соглашение</a>
-        <a class="footer__link" href="#">Помощь</a>
-        <a class="footer__link" href="#">Контакты</a>
-      </div>
+      <div class="footer__grid">
+        <section class="footer__col" aria-label="Раздел: КАТАЛОГ">
+          <h2 class="footer__title">Каталог</h2>
+          <ul class="footer__list">
+            <li class="footer__item"><a class="footer__link" href="#">Главная</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Мужчинам</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Женщинам</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Детям</a></li>
+          </ul>
+        </section>
 
-      <p class="footer__meta">© {{ year }} Tapir. Тестовое задание (Nuxt 4 + TypeScript).</p>
+        <section class="footer__col" aria-label="Раздел: ПОКУПКА">
+          <h2 class="footer__title">Покупка</h2>
+          <ul class="footer__list">
+            <li class="footer__item"><a class="footer__link" href="#">Доставка</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Оплата</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Возврат</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">FAQ</a></li>
+          </ul>
+        </section>
+
+        <section class="footer__col" aria-label="Раздел: ИНФОРМАЦИЯ">
+          <h2 class="footer__title">Информация</h2>
+          <ul class="footer__list">
+            <li class="footer__item"><a class="footer__link" href="#">О компании</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Политика конфиденциальности</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Пользовательское соглашение</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Публичная оферта</a></li>
+          </ul>
+        </section>
+
+        <section class="footer__col" aria-label="Раздел: КОНТАКТЫ">
+          <h2 class="footer__title">Контакты</h2>
+          <ul class="footer__list">
+            <li class="footer__item"><a class="footer__link" href="#">+7 (000) 000‑00‑00</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">hello@example.com</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Москва</a></li>
+            <li class="footer__item"><a class="footer__link" href="#">Реквизиты</a></li>
+          </ul>
+        </section>
+      </div>
     </div>
   </footer>
 </template>
-
-<script setup lang="ts">
-const year = new Date().getFullYear()
-</script>
 
