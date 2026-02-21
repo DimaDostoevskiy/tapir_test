742e4a8440635f159121e032f208dbfbc0183eac
dos <dosdima89@gmail.com>
2026-02-21 10:11:23 +0300

    auth/middleware

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,11 +1,29 @@
-export default defineNuxtRouteMiddleware(async (from, to) => {
-    const token = typeof to.query.token === 'string' ? to.query.token.trim() : ``
+export default defineNuxtRouteMiddleware(async (to) => {
+    const isAdminRoute = to.path.startsWith('/admin')
+    const tokenFromQuery = typeof to.query.token === 'string' ? to.query.token.trim() : ''
 
-    if (token) {
-        await $fetch<{ role: string, name: string }>('/api/auth/me', {
-            query: { token },
-        }).then(res => {
-            console.log(res);
-        })
+
+    try {
+        const cookieUser = useCookie<{ role: string, name: string } | null>('auth_user', { sameSite: 'lax' })
+
+        if (tokenFromQuery) {
+            const res = await $fetch<{ role: string, name: string }>('/api/auth/me', {
+                query: { token: tokenFromQuery },
+            })
+
+            cookieUser.value = {
+                role: res.role,
+                name: res.name,
+            }
+        }
+
+
+        console.log(cookieUser.value);
+
+        if (isAdminRoute && cookieUser.value.role !== 'ADMIN') {
+            return navigateTo('/')
+        }
+    } catch {
+        return navigateTo('/')
     }
 })
