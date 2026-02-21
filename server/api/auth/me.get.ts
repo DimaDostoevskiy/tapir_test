e5b72b01855269c39aaca72398146ed953a0279f
dos <dosdima89@gmail.com>
2026-02-21 10:55:23 +0300

    auth/middleware

--- server/api/auth/me.get.ts
+++ server/api/auth/me.get.ts
@@ -1,8 +1,6 @@
-
 import { getQuery } from 'h3'
 
 export default defineEventHandler(async (event) => {
-    // const runtimeConfig = useRuntimeConfig()
     const query = getQuery(event)
     const token = typeof query.token === 'string' ? query.token.trim() : ''
 
@@ -16,6 +14,8 @@ export default defineEventHandler(async (event) => {
             'Authorization': token,
         },
     }).then(res => {
+        console.log(res?.user?.Role)
+        console.log(res?.user?.Name)
         return {
             role: res?.user?.Role?.toString() || incognitoUser.role,
             name: res?.user?.Name?.toString() || incognitoUser.name,
