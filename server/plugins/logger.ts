41a27f47ae925429af92722c8aa8010787dcaf90
dos <dosdima89@gmail.com>
2026-02-27 15:58:51 +0300

    logger

--- server/plugins/logger.ts
+++ server/plugins/logger.ts
@@ -1,15 +1,18 @@
-import { defineNitroPlugin } from '#imports'
+import {defineNitroPlugin} from '#imports'
 
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('request', (event) => {
-        console.info('[REQ]', event.method, event.path)
+        if (event.path == `/favicon.ico` || event.path == `/avatar.jpg`) return
+        console.info(`[${new Date().toTimeString().split(' ')[0]}] [REQ]`, `-->`, event.method, event.path)
     })
 
     nitroApp.hooks.hook('afterResponse', (event) => {
-        console.info('[RES]', event.method, event.path, event.node.res.statusCode)
+        if (event.path == `/favicon.ico` || event.path == `/avatar.jpg`) return
+        console.info(`[${new Date().toTimeString().split(' ')[0]}] [RES]`, event.node.res.statusCode, event.method, `/blog` + event.path, )
     })
 
-    nitroApp.hooks.hook('error', (error, { event }) => {
-        console.error('[ERR]', event?.method, event?.path, error)
+    nitroApp.hooks.hook('error', (error, {event}) => {
+        if (event?.path == `/favicon.ico` || event?.path == `/avatar.jpg`) return
+        console.error(`[${new Date().toTimeString().split(' ')[0]}] [ERR]`, event?.method, event?.path, error)
     })
 })
